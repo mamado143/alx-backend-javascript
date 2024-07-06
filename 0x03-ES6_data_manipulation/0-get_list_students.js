fe0b72af0ca871bb873c625e2a13127035392390
mamado143 <midodahir123@gmail.com>
2024-07-06 08:27:53 +0000

    Solve: problem 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+/**
+ * create function named getListStudents.
+ * function - getListStudnets
+ * Returns: id, firstName , location.
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
